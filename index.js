aa0c212deb494d94248093b18ff786628231841c
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-02-01 20:22:57 -0600

    commit tree 621731d5151abc03999617ae39e45dbdb1b4e53b

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+  else if (distance > 2500) {
+    return "No can do."
+  }
+  else if (distance > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+}
+function ternaryCheckCity(city) {
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case tip = "generous":
+      return "Thank you so much.";
+      break;
+    case tip = "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
